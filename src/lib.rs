acdcc32f4e94de610795b375d21e798b12939541
kirfed <kirfed@gmail.com>
2020-01-13 17:12:36 +0300

    size and root parameters

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,26 @@ struct Links<T: Clone> {
 #[derive(Default)]
 pub struct MappedTree<T: Clone + Eq + Hash> {
     links_by_obj: HashMap<T, Links<T>>,
+    size: usize,
 }
 
 impl<T: Clone + Eq + Hash> MappedTree<T> {
     pub fn new() -> Self {
         MappedTree {
             links_by_obj: HashMap::new(),
+            size: 0,
         }
     }
 
     pub fn with_capacity(capacity: usize) -> Self {
         MappedTree {
             links_by_obj: HashMap::with_capacity(capacity),
+            size: 0,
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
     }
 
     fn links(&self, obj: &T) -> Option<&Links<T>> {
@@ -44,6 +51,37 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
         None
     }
 
+    pub fn root(&self) -> Option<T> {
+        if let Some(next) = self.links_by_obj.keys().next() {
+            let mut next = next.clone();
+            while let Some(parent) = self.parent(&next) {
+                next = parent.clone();
+            }
+            return Some(next);
+        }
+
+        None
+    }
+
+    pub fn reset_root(&mut self, obj: &T) -> Option<T> {
+        if let Some(old_root) = self.root() {
+            let children = self.children(&old_root).unwrap().clone();
+            for child in &children {
+                self.links_by_obj.get_mut(child).unwrap().parent = Some(obj.clone());
+            }
+
+            self.links_by_obj.remove(&old_root);
+            let links = Links {
+                parent: None,
+                children: children,
+            };
+            self.links_by_obj.insert(obj.clone(), links);
+            return Some(old_root);
+        }
+
+        None
+    }
+
     pub fn insert(&mut self, obj: &T, parent: &T) {
         if self.links_by_obj.contains_key(obj) {
             panic!("mapped tree MUST contain UNIQUE elements only");
@@ -52,7 +90,7 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
         let parent_links = self
             .links_by_obj
             .get_mut(parent)
-            .expect("mapped tree don't contain specified parent");
+            .expect("mapped tree doesn't contain specified parent");
 
         parent_links.children.push(obj.clone());
 
@@ -63,6 +101,7 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
                 children: Vec::new(),
             },
         );
+        self.size += 1;
     }
 
     fn remove_children_without_links(&mut self, obj: &T) {
@@ -72,9 +111,11 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
                 self.remove_children(&child);
             }
 
+            let children_count = children.len();
             for child in &children {
                 self.links_by_obj.remove(&child);
             }
+            self.size -= children_count;
         }
     }
 
@@ -88,6 +129,7 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
     pub fn remove(&mut self, obj: &T) -> bool {
         self.remove_children(obj);
         if let Some(links) = self.links_by_obj.remove(obj) {
+            self.size -= 1;
             if let Some(parent) = &links.parent {
                 let parent_links = self.links_by_obj.get_mut(parent).unwrap();
                 let obj_index = parent_links
@@ -110,8 +152,11 @@ impl<T: Clone + Eq + Hash> MappedTree<T> {
 
 #[cfg(test)]
 mod tests {
+    use super::MappedTree;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn size() {
+        let vals = vec![0, 2, 4, 6, 8, 9];
+        let map: MappedTree<i32> = MappedTree::new();
     }
 }
